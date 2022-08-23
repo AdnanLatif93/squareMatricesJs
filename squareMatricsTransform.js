78b732cdb1da141f7e890aa9b423ca3b3d4ada2b
Adnan Faisal Latif <adnan.faisal@postex.pk>
2022-08-23 11:12:52 +0500

    squareMatrics Tranform using map method.

--- squareMatricsTransform.js
+++ squareMatricsTransform.js
@@ -5,6 +5,23 @@ let mat1 = [
     [4, 4, 4, 4]
 ];
 
-console.table(mat1[0].map((obj , colIndex) => {
-     return mat1.map(obj2 => obj2[colIndex])
+console.table(mat1.map((obj , colIndex, array) => {
+     return mat1.map(obj2 =>  obj2[colIndex])
 }))
+
+console.table(mat1.map(function(element, index, array){
+   return mat1.map(function(element2, index2, array2){
+       return element2[index]
+    })
+}));
+
+var transpose = [];
+for (var i = 0; i < mat1.length; i++) {
+    transpose.push([]);
+    for (var j = 0; j < mat1.length; j++) {
+        transpose[i].push(mat1[j][i])
+    }
+}
+console.table((transpose)); 
+
+
